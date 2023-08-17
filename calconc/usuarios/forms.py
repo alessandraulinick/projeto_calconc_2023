d404cdc547d31bb6f73a92c37bc1e59523486cbc
ALESSANDRA ULINICK <aleeulinick@gmail.com>
2023-08-17 20:42:58 -0300

    mudancas 2

--- calconc/usuarios/forms.py
+++ calconc/usuarios/forms.py
@@ -36,7 +36,74 @@ class TipoAgregadoForms(forms.ModelForm):
 
 class AgregadoForms(forms.ModelForm):
     tipo_agregado = forms.ModelChoiceField(queryset=TipoAgregado.objects.all(), label="Tipo de Agregado")
+    fornecedor = forms.ModelChoiceField(queryset=Fornecedor.objects.all(), label="Fornecedor")
 
     class Meta:
         model = Agregado
         fields = ['tipo_agregado', 'nome', 'pen_6_30_mm', 'pen_4_80_mm', 'pen_2_40_mm', 'pen_1_20_mm', 'pen_600_um', 'pen_300_um', 'pen_150_um', 'pen_75_um', 'fundo', 'umidade', 'massa_especifica']
+
+    def clean_pen_6_30_mm(self):
+        pen_6_30_mm = self.cleaned_data.get('pen_6_30_mm')
+        if pen_6_30_mm <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_6_30_mm
+
+    def clean_pen_4_80_mm(self):
+        pen_4_80_mm = self.cleaned_data.get('pen_4_80_mm')
+        if pen_4_80_mm <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_4_80_mm
+
+    def clean_pen_2_40_mm(self):
+        pen_2_40_mm = self.cleaned_data.get('pen_2_40_mm')
+        if pen_2_40_mm <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_2_40_mm
+
+    def clean_pen_1_20_mm(self):
+        pen_1_20_mm = self.cleaned_data.get('pen_1_20_mm')
+        if pen_1_20_mm <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_1_20_mm
+
+    def clean_pen_600_um(self):
+        pen_600_um = self.cleaned_data.get('pen_600_um')
+        if pen_600_um <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_600_um
+
+    def clean_pen_300_um(self):
+        pen_300_um = self.cleaned_data.get('pen_300_um')
+        if pen_300_um <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_300_um
+
+    def clean_pen_150_um(self):
+        pen_150_um = self.cleaned_data.get('pen_150_um')
+        if pen_150_um <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_150_um
+
+    def clean_pen_75_um(self):
+        pen_75_um = self.cleaned_data.get('pen_75_um')
+        if pen_75_um <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return pen_75_um
+
+    def clean_fundo(self):
+        fundo = self.cleaned_data.get('fundo')
+        if fundo <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return fundo
+
+    def clean_umidade(self):
+        umidade = self.cleaned_data.get('umidade')
+        if umidade <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return umidade
+
+    def clean_massa_especifica(self):
+        massa_especifica = self.cleaned_data.get('massa_especifica')
+        if massa_especifica <= 0:
+            raise forms.ValidationError("O valor deve ser maior que zero.")
+        return massa_especifica
