a7f36628d32464e348b6570953f2555c25332023
ALESSANDRA ULINICK <aleeulinick@gmail.com>
2023-10-19 18:48:52 -0300

    mudança html e css

--- calconc/djangoo_calconc/urls.py
+++ calconc/djangoo_calconc/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-angfrom usuarios import views
+from usuarios import views
 from django.conf.urls import handler404
 
 
